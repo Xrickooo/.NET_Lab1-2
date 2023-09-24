784a67f09f6d417531721ad1717f5749bf88c53e
Xrickooo <89274575+Xrickooo@users.noreply.github.com>
2023-09-24 19:28:30 +0300

    zvit and fix

--- Lab1/ConsoleApp.cs
+++ Lab1/ConsoleApp.cs
@@ -6,7 +6,7 @@ namespace Lab1
 {
     public class ConsoleApp
     {
-        private MyDictionary<int, string> _dict;
+        private readonly MyDictionary<int, string> _dict;
 
         public ConsoleApp()
         {

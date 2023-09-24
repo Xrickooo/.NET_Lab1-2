e3665ca2dc36fbea1bfe990031ef5d127724ec46
Xrickooo <89274575+Xrickooo@users.noreply.github.com>
2023-09-24 18:57:01 +0300

    ConsoleApp added

---
+++ Lab1/ConsoleApp.cs
@@ -0,0 +1,136 @@
+﻿using Lab1;
+using System;
+using System.Collections.Generic;
+
+namespace Lab1
+{
+    public class ConsoleApp
+    {
+        private MyDictionary<int, string> dict;
+
+        public ConsoleApp()
+        {
+            dict = new MyDictionary<int, string>();
+        }
+
+        public void Run()
+        {
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("MyDictionary Console App");
+                Console.WriteLine("========================");
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("1. Add Key-Value Pair");
+                Console.WriteLine("2. Search for a Key");
+                Console.WriteLine("3. Remove a Key");
+                Console.WriteLine("4. Display All Key-Value Pairs");
+                Console.WriteLine("5. Clear Dictionary");
+                Console.WriteLine("6. Exit");
+                Console.Write("Enter your choice (1-6): ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.WriteLine("Add Key-Value Pair");
+                            Console.WriteLine("===================");
+                            Console.Write("Enter Key (int): ");
+                            if (int.TryParse(Console.ReadLine(), out int key))
+                            {
+                                Console.Write("Enter Value (string): ");
+                                string value = Console.ReadLine();
+                                dict.Add(key, value);
+                                Console.WriteLine("\nKey-Value pair added successfully.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nInvalid input. Key must be an integer.");
+                            }
+                            break;
+
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("Search for a Key");
+                            Console.WriteLine("=================");
+                            Console.Write("Enter Key to search for: ");
+                            if (int.TryParse(Console.ReadLine(), out int searchKey))
+                            {
+                                string result = dict.Search(searchKey);
+                                Console.WriteLine($"\nSearch result: {result}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nInvalid input. Key must be an integer.");
+                            }
+                            break;
+
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("Remove a Key");
+                            Console.WriteLine("============");
+                            Console.Write("Enter Key to remove: ");
+                            if (int.TryParse(Console.ReadLine(), out int removeKey))
+                            {
+                                bool removed = dict.Remove(removeKey);
+                                if (removed)
+                                {
+                                    Console.WriteLine("\nKey-Value pair removed successfully.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("\nKey not found in the dictionary.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nInvalid input. Key must be an integer.");
+                            }
+                            break;
+
+                        case 4:
+                            Console.Clear();
+                            Console.WriteLine("All Key-Value Pairs");
+                            Console.WriteLine("===================");
+                            foreach (var item in dict)
+                            {
+                                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+                            }
+                            break;
+
+                        case 5:
+                            Console.Clear();
+                            Console.WriteLine("Clear Dictionary");
+                            Console.WriteLine("================");
+                            dict.Clear();
+                            Console.WriteLine("\nDictionary cleared.");
+                            break;
+
+                        case 6:
+                            Console.Clear();
+                            Console.WriteLine("Exiting the application.");
+                            return;
+
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("\nInvalid choice. Please select a valid option.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nInvalid input. Please enter a number.");
+                }
+
+                Console.Write("\nPress Enter to continue...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -8,46 +8,10 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            var dict = new MyDictionary<int, string>();
-            dict.Add(1, "One");
-            dict.Add(2, "Two");
-            dict.Add(3, "Three");
-            dict.Add(4, "Four");
-            dict.Add(5, "Five");
-            dict.TryAdd(new Item<int, string>(101, "One hundred and one"));
-
-            foreach (var item in dict)
-            {
-                Console.WriteLine(item);
-            }
-
-            //Console.WriteLine(dict.Search(7));
-            Console.WriteLine(dict.Search(3));
-            Console.WriteLine(dict.Search(101));
-
-            //dict.Remove(7);
-            dict.Remove(3);
-            dict.Remove(1);
-            dict.Remove(101);
-
-            foreach (var item in dict)
-            {
-                Console.WriteLine(item);
-            }
-
-            KeyValuePair<int, string>[] array = new KeyValuePair<int, string>[3];
-
-            dict.CopyTo(array, 0);
-
-            foreach (var kvp in array)
-            {
-                Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
-            }
-
-            dict.Clear();
-
-            Console.WriteLine();
-            Console.ReadLine();
+            var app = new ConsoleApp();
+            app.Run();
         }
     }
 }
+
+
